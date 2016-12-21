fd2e988001db75aadf5192c7e4190c58ab1457f5
fbenavid <fbenalvarez92@gmail.com>
2016-12-21 15:36:52 -0500

    new shirt buttons working

--- script/models.js
+++ script/models.js
@@ -29,7 +29,7 @@ function draw() {
     document.getElementById('aqua').addEventListener('click', colorChange);
     document.getElementById('white').addEventListener('click', colorChange);
 
-    document.getElementById('frontBack').addEventListener('click',frontBackChange);
+    document.getElementById('frontBack').addEventListener('click', frontBackChange);
 
     /*
      * Changing shirt colors
@@ -147,79 +147,37 @@ function draw() {
             cnv.ctx.drawImage(img, 0, 0);
             cnv.ctx.save();
         });
-
-        // if (color != 'white') {
-        //     // Defining the rectangular area of pixels to extract (full canvas)
-        //     var extract_rect = new Rectangle(0, 0);
-        //     extract_rect.set_width(img.width);
-        //     extract_rect.set_height(img.height);
-        //
-        //     // Extraction of pixels data
-        //     var imgData = cnv.ctx.getImageData(extract_rect.get_x1(), extract_rect.get_y1(), extract_rect.get_width(), extract_rect.get_height());
-        //
-        //     /*clearing rectangle*/
-        //     cnv.ctx.clearRect(0, 0, cnv.get_width(), cnv.get_height());
-        //
-        //     // The length of the data is 4 times the number of pixels, each pixels takes 4 bytes of data
-        //     console.log('Nb of pixels extracted : ', imgData.data.length / 4);
-        //
-        //     // Backup the pixel data somewhere
-        //     var pixel_backup = [];
-        //     for (var i = 0; i <= imgData.data.length - 4; i++) {
-        //         pixel_backup.push(imgData.data[i]);
-        //     }
-        //
-        //     // Manipulate the pixels
-        //     // If the pixel is close to white, change it to transparent
-        //     // Parse each pixel one by one and change its rgba components
-        //     // Here, the i control variable of the loop is the index of the red component of each pixel
-        //
-        //     for (i = 0; i <= imgData.data.length - 4; i += 4) {
-        //
-        //         if (color == 'red') {
-        //             imgData.data[i + 0] = pixel_backup[i + 0]; // Restore red
-        //             imgData.data[i + 1] = 0; // Remove Green
-        //             imgData.data[i + 2] = 0; // Remove blue
-        //             color = 'red';
-        //         }
-        //         else if (color == 'green') {
-        //             imgData.data[i + 0] = 0; // Remove red
-        //             imgData.data[i + 1] = pixel_backup[i + 2]; // Restore green
-        //             imgData.data[i + 2] = 0; // Remove Blue
-        //             color = 'green';
-        //         }
-        //         else if (color == 'blue') {
-        //             imgData.data[i + 0] = 0; // Remove red
-        //             imgData.data[i + 1] = 0; // Remove green
-        //             imgData.data[i + 2] = pixel_backup[i + 2]; // Restore Blue
-        //             color = 'blue';
-        //         }
-        //         else if (color == 'yellow') {
-        //             imgData.data[i + 0] = pixel_backup[i + 0]; // Restore red
-        //             imgData.data[i + 1] = pixel_backup[i + 1]; // Restore green  //yellow
-        //             imgData.data[i + 2] = 0; // Remove Blue
-        //             color = 'yellow';
-        //         }
-        //         else if (color == 'aqua') {
-        //             imgData.data[i + 0] = 0; // Remove red
-        //             imgData.data[i + 1] = pixel_backup[i + 1]; // Restore green  //aqua
-        //             imgData.data[i + 2] = pixel_backup[i + 2]; // Restore Blue
-        //             color = 'aqua';
-        //         }
-        //         else if (color == 'hotPink') {
-        //             imgData.data[i + 0] = pixel_backup[i + 0]; // Remove red
-        //             imgData.data[i + 1] = 0; // Remove green                     //hotPink
-        //             imgData.data[i + 2] = pixel_backup[i + 2]; // Restore Blue
-        //             color = 'hotPink';
-        //         }
-        //
-        //         imgData.data[i + 3] = pixel_backup[i + 3]; // Restore alpha
-        //     }
-        //
-        //     cnv.ctx.putImageData(imgData, 0, 0);
-        //     console.log(color);
-        //     console.log('After putting pixels back : ', imgData.data.length / 4);
-        // }
     }
+
+
+    for (var i = 0; i < document.getElementsByClassName('shirtSelection').length; i++) {
+        document.getElementsByClassName('shirtSelection')[i].addEventListener('click', changeShirt);
+    }
+
+
+    function changeShirt() {
+        console.log('in changeShirt function')
+
+        /*clearing rectangle*/
+        cnv.ctx.clearRect(0, 0, cnv.get_width(), cnv.get_height());
+
+        shirtType = this.id;
+        img.src = 'image/' + shirtType + document.getElementById('frontBack').innerHTML.trim() + '.png';
+        img.addEventListener('load', function () {
+            //when image is loaded, event is triggered
+            console.log('t-shirt image is loaded, put it on the canvas');
+            cnv.ctx.drawImage(img, 0, 0);
+            cnv.ctx.save();
+        });
+    }
+
+    /*function hexToRgb(hex) {
+     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+     return result ? {
+     r: parseInt(result[1], 16),
+     g: parseInt(result[2], 16),
+     b: parseInt(result[3], 16)
+     } : null;
+     }*/
 }
 
